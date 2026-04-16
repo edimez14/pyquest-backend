5651f7aab480bf434994c3b8aca53079cd933ff3
edimez14 <edimez14@gamil.com>
2026-04-15 21:03:17 -0500

    feat(edizon): estandarizar respuestas de la api

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -4,22 +4,40 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { map, Observable } from 'rxjs';
 
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<
   T,
-  { success: boolean; data: T }
+  StandardSuccessResponse<T>
 > {
   intercept(
     _context: ExecutionContext,
     next: CallHandler<T>,
-  ): Observable<{ success: boolean; data: T }> {
+  ): Observable<StandardSuccessResponse<T>> {
+    const http = _context.switchToHttp();
+    const request = http.getRequest<Request>();
+    const response = http.getResponse<Response>();
+
     return next.handle().pipe(
       map((data: T) => ({
         success: true,
+        statusCode: response.statusCode,
+        path: request.url,
+        timestamp: new Date().toISOString(),
+        message: 'Solicitud procesada correctamente',
         data,
       })),
     );
   }
+}
+
+interface StandardSuccessResponse<T> {
+  success: true;
+  statusCode: number;
+  path: string;
+  timestamp: string;
+  message: string;
+  data: T;
 }
